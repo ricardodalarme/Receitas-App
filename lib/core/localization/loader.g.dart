9a973dc8c1cf43f20a98e113008ab650d13e1543
ricardodalarme <ricardoweasley@hotmail.com>
2021-07-31 12:09:56 -0300

    Adicionado suporte à múltiplos idiomas

---
+++ lib/core/localization/loader.g.dart
@@ -0,0 +1,46 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: prefer_single_quotes
+
+import 'dart:ui';
+
+import 'package:easy_localization/easy_localization.dart' show AssetLoader;
+
+class CodegenLoader extends AssetLoader {
+  const CodegenLoader();
+
+  @override
+  Future<Map<String, dynamic>> load(String path, Locale locale) {
+    return Future.value(mapLocales[locale.toString()]);
+  }
+
+  static const Map<String, dynamic> en_US = {
+    "welcomeBack": "Welcome Back!",
+    "pleaseLogin": "Please login to continue.",
+    "emailAddress": "Email address",
+    "password": "Password",
+    "forgotPassowrd": "Forgot password?",
+    "login": "Login",
+    "facebookLogin": "Continue with Facebook",
+    "newUser": "New to Scratch?",
+    "createAccount": "Create Account Here",
+    "myKitchen": "My Kitchen",
+    "settings": "Settings",
+    "followers": "Followers",
+    "likes": "Likes",
+    "recipes": "Recipes",
+    "saved": "Saved",
+    "following": "Following",
+    "backProfile": "Back to My Profile",
+    "myRecipes": "My Recipes",
+    "addNews": "Add New",
+    "cook": "Cook",
+    "ingredients": "Ingredients",
+    "backRecipes": "Back to My Recipes",
+    "editRecipe": "Edit recipe",
+    "recipeName": "Recipe Name",
+    "gallery": "Gallery",
+    "howCook": "How to cook"
+  };
+  static const Map<String, Map<String, dynamic>> mapLocales = {"en_US": en_US};
+}
---
+++ lib/core/localization/locale_keys.g.dart
@@ -0,0 +1,31 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const welcomeBack = 'welcomeBack';
+  static const pleaseLogin = 'pleaseLogin';
+  static const emailAddress = 'emailAddress';
+  static const password = 'password';
+  static const forgotPassowrd = 'forgotPassowrd';
+  static const login = 'login';
+  static const facebookLogin = 'facebookLogin';
+  static const newUser = 'newUser';
+  static const createAccount = 'createAccount';
+  static const myKitchen = 'myKitchen';
+  static const settings = 'settings';
+  static const followers = 'followers';
+  static const likes = 'likes';
+  static const recipes = 'recipes';
+  static const saved = 'saved';
+  static const following = 'following';
+  static const backProfile = 'backProfile';
+  static const myRecipes = 'myRecipes';
+  static const addNews = 'addNews';
+  static const cook = 'cook';
+  static const ingredients = 'ingredients';
+  static const backRecipes = 'backRecipes';
+  static const editRecipe = 'editRecipe';
+  static const recipeName = 'recipeName';
+  static const gallery = 'gallery';
+  static const howCook = 'howCook';
+
+}
